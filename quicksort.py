820b18b7dc15dd8b67e0b40bdc7da440df44a000
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-07-22 14:51:33 +0200

    quicksort works, but its dirty and does too many recursions

--- quicksort.py
+++ quicksort.py
@@ -1,24 +1,27 @@
-arr = [8, 2, 4, 7, 1, 3, 9, 6, 5]
+#arr = [8, 2, 4, 7, 1, 3, 9, 6, 5]
+arr = [64, 34, 25, 12, 22, 11, 90, 110]
 # def median_of_3(arr):
     
 
 def quicksort(arr, start, end):
    """
-   choosing pivot by the median-of-the-three method
-   not yet
    just by last element
    """
    pivot = arr[end]
    n = end
-   i = -1
+   i = start - 1
    swap = 0
 
    print(10*"=", " Quicksort ", 10*"=")
+   print(f"{start=}, {end=}")
    print(arr)
    print("pivot is:", pivot)
    
-   for j in range(end):
-       if arr[j] <= pivot:
+   if start >= end:
+       return arr
+
+   for j in range(start, end):
+       if arr[j] < pivot:
            i += 1
            print(f"{i=}, {j=}, {arr[j]=}, {arr[i]=}")
            swap = arr[j]
@@ -26,10 +29,10 @@ def quicksort(arr, start, end):
            arr[i] = swap
    arr[end] = arr[i+1]
    arr[i+1] = pivot
-   
-   print(arr)
+   print("front")
+   arr = quicksort(arr, start, i)
+   print("rear")
+   arr = quicksort(arr, i+2, len(arr)-1)
    return arr
 
-arr2 = quicksort(arr, 0, len(arr)-1)
-arr3 = quicksort(arr, 0, 3)
-arr4 = quicksort(arr, 0, 1)
+arr = quicksort(arr, 0, len(arr)-1)
