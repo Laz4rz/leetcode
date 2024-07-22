58a1fb02a763c67a6c240f3f2321c76a8bf8b1ba
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-07-22 15:01:52 +0200

    quicksort: completed

--- quicksort.py
+++ quicksort.py
@@ -1,24 +1,25 @@
-#arr = [8, 2, 4, 7, 1, 3, 9, 6, 5]
-arr = [64, 34, 25, 12, 22, 11, 90, 110]
-# def median_of_3(arr):
-    
+arr = [8, 2, 4, 7, 1, 3, 9, 6, 5]
+#arr = [64, 34, 25, 12, 22, 11, 90, 110]
+
+recursion_depth = 0
 
 def quicksort(arr, start, end):
    """
    just by last element
    """
+   if start >= end:
+       return arr
    pivot = arr[end]
    n = end
    i = start - 1
    swap = 0
 
-   print(10*"=", " Quicksort ", 10*"=")
+   global recursion_depth
+   recursion_depth += 1
+   print(10*"=", f" Quicksort: {recursion_depth} ", 10*"=")
    print(f"{start=}, {end=}")
    print(arr)
-   print("pivot is:", pivot)
-   
-   if start >= end:
-       return arr
+   print("pivot is:", pivot) 
 
    for j in range(start, end):
        if arr[j] < pivot:
@@ -29,10 +30,11 @@ def quicksort(arr, start, end):
            arr[i] = swap
    arr[end] = arr[i+1]
    arr[i+1] = pivot
-   print("front")
-   arr = quicksort(arr, start, i)
-   print("rear")
-   arr = quicksort(arr, i+2, len(arr)-1)
+
+   quicksort(arr, start, i)
+   quicksort(arr, i+2, end)
    return arr
 
 arr = quicksort(arr, 0, len(arr)-1)
+
+print(f"\nFinal {arr}")
