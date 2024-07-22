2b5e14e9d091dc67344898743b3953a96539954d
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-07-22 20:34:08 +0200

    quicksort: add testing for complexity

--- quicksort.py
+++ quicksort.py
@@ -1,7 +1,15 @@
+import random 
+
+random.seed(42)
+
+def random_array(length, bottom, top):
+    return [random.randint(bottom, top) for i in range(length)]
+
 arr = [8, 2, 4, 7, 1, 3, 9, 6, 5]
 #arr = [64, 34, 25, 12, 22, 11, 90, 110]
 
 recursion_depth = 0
+n_operations = 0
 
 def quicksort(arr, start, end):
    """
@@ -9,15 +17,19 @@ def quicksort(arr, start, end):
    """
    original = arr[:] # for shallow copy
    if start >= end:
-      assert all([el in original for el in arr]), "dang we lost someone"
+      # assert on sorting invariant:
+      # all elements have to match before and after, only position changes
+      assert set(original) == set(arr), "dang we lost someone"
       return arr
    pivot = arr[end]
    n = end
    i = start - 1
    swap = 0
 
+   global n_operations
    global recursion_depth
    recursion_depth += 1
+
    print(10*"=", f" Quicksort: {recursion_depth} ", 10*"=")
    print(f"{start=}, {end=}")
    print(arr)
@@ -30,6 +42,7 @@ def quicksort(arr, start, end):
            swap = arr[j]
            arr[j] = arr[i]
            arr[i] = swap
+       n_operations += 1
    arr[end] = arr[i+1]
    arr[i+1] = pivot
 
@@ -40,5 +53,11 @@ def quicksort(arr, start, end):
 arr = quicksort(arr, 0, len(arr)-1)
 
 print(f"\nFinal {arr}")
+print(f"Total number of operations: {n_operations}")
 
-
+# testing suite
+recursion_depth = 0
+n_operations = 0 
+for i in range(100):
+    quicksort(random_array(100, 0, 100), 0, 99)
+print(f"Average number of operations: {n_operations/100}")
