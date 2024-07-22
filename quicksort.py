a423b2358dfbf5f0463385b5e5859f7857f739e2
Laz4rz <62252332+Laz4rz@users.noreply.github.com>
2024-07-22 12:42:04 +0200

    quicksort working non-recursive

--- quicksort.py
+++ quicksort.py
@@ -2,13 +2,14 @@ arr = [8, 2, 4, 7, 1, 3, 9, 6, 5]
 # def median_of_3(arr):
     
 
-def quicksort(arr):
+def quicksort(arr, start, end):
    """
    choosing pivot by the median-of-the-three method
    not yet
+   just by last element
    """
-   pivot = arr[-1]
-   n = len(arr)
+   pivot = arr[end]
+   n = end
    i = -1
    swap = 0
 
@@ -16,17 +17,19 @@ def quicksort(arr):
    print(arr)
    print("pivot is:", pivot)
    
-   for j in range(n):
+   for j in range(end):
        if arr[j] <= pivot:
            i += 1
            print(f"{i=}, {j=}, {arr[j]=}, {arr[i]=}")
            swap = arr[j]
            arr[j] = arr[i]
            arr[i] = swap
-#   arr[-1] = arr[i+1]
- #  arr[i+1] = pivot
+   arr[end] = arr[i+1]
+   arr[i+1] = pivot
    
    print(arr)
    return arr
 
-quicksort(arr)
+arr2 = quicksort(arr, 0, len(arr)-1)
+arr3 = quicksort(arr, 0, 3)
+arr4 = quicksort(arr, 0, 1)
